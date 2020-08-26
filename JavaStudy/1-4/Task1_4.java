d044cc6d4d70dd3c825d4bd0ca3e19f98ccb8afe
Yoshiki-s <acoustic.vio64@gmail.com>
2020-08-26 21:52:46 +0900

    再提出課題提出テスト
    Task1_4.javaの作成

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -35,16 +35,17 @@ public class Task1_4 {
       }
 
       // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-      else if (name.equals("alice"))
+      else if (name.equals("alice")){
         System.out.println(CONST_MSG_ERROR_PASS);
-
+      }
       // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-      else if (pass.equals("pass"))
+      else if (pass.equals("pass")){
         System.out.println(CONST_MSG_ERROR_NAME);
-
+      }
       // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
-      else if (name.equals("alice") && pass.equals("pass") )
+      else {
         System.out.println(CONST_MSG_ERROR_INPUT);
   }
 
+}
 }
